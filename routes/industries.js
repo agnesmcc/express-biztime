e94db2e2c91cb181e823eb1aa2e165f333040d60
Agnes <agnes.mccall5@gmail.com>
2024-07-21 20:30:38 -0400

    add route for adding company to industry

--- routes/industries.js
+++ routes/industries.js
@@ -4,6 +4,17 @@ const router = express.Router();
 const db = require('../db');
 const slugify = require('slugify');
 
+router.get('/', async (req, res, next) => {
+    try {
+        const results = await db.query(
+            `SELECT code, industry
+             FROM industries`);
+        return res.json({industries: results.rows});
+    } catch (err) {
+        return next(err);
+    }
+});
+
 router.post('/', async (req, res, next) => {
     try {
         const { industry } = req.body;
@@ -12,6 +23,19 @@ router.post('/', async (req, res, next) => {
             `INSERT INTO industries (code, industry)
              VALUES ($1, $2)
              RETURNING code, industry`, [code, industry]);
+        return res.status(201).json({industry: results.rows[0]});
+    } catch (err) {
+        return next(err);
+    }
+});
+
+router.post('/add_company', async (req, res, next) => {
+    try {
+        const { industry_code, company_code } = req.body;
+        const results = await db.query(
+            `INSERT INTO industries_companies (industry, company)
+             VALUES ($1, $2)
+             RETURNING industry, company`, [industry_code, company_code]);
         return res.status(201).json({industry: results.rows[0]});
     } catch (err) {
         return next(err);
